df091331437c62352c8c7744e0879a6fc29b383b
FairylandX <53886721+FairylandX@users.noreply.github.com>
2020-02-02 00:37:21 +0800

    MAC
    Adjust for MAC system model

--- Simple face recognition/face_recognition.py
+++ Simple face recognition/face_recognition.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 img = cv2.imread('1.jpg')
 d = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-f = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
+f = cv2.CascadeClassifier('/opt/anaconda3/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml')
 a = f.detectMultiScale(d,1.5,2)
 for(x,y,w,h) in a:
     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),4)
